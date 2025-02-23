10d81f361db19da464aa029d0d668538b7ee177c
Sahil <sahilluthra495@gmail.com>
2025-02-23 13:03:29 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, HashRouter } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Header from "./components/header/header";
 import Login from "./pages/auth/Login";
@@ -30,7 +30,7 @@ const App = () => {
       {loading ? (
         <Loading />
       ) : (
-        <BrowserRouter>
+        <HashRouter>
           <Header isAuth={isAuth} />
           <Routes>
             <Route path="/" element={<Home />} />
@@ -91,7 +91,7 @@ const App = () => {
             />
           </Routes>
           <Footer />
-        </BrowserRouter>
+        </HashRouter>
       )}
     </>
   );
